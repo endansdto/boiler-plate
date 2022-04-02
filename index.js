1058b6e3d3bf74b416cb66eed48ea012dac05930
endansdto <dudwls020705@naver.com>
2022-04-02 23:38:59 +0900

    개인정보보호

--- index.js
+++ index.js
@@ -2,13 +2,31 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const { User } = require('./models/User')
+const bodyparser = require('body-parser')
+
+const config = require('./config/key')
+
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyparser.json())
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://root:el1lUw8O238tydZu@cluster0.azmcz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+const { use } = require('express/lib/application')
+mongoose.connect(config.mongoURI)
     .then(()=>console.log('MongoDB Connected...'))
     .catch(err=>console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.send('helloo world')
+})
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body)
+  user.save((err, doc) => {
+    if (err)
+      return res.json({success: false, err})
+    return res.status(200).json({success:true})
+  })
 })
 
 app.listen(port, () => console.log(`example app listening on port ${port}`))
